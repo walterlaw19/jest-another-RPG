3d139335cffdb454e85159b3500e3be1464c6b60
SC2021 <walterlaw19@gmail.com>
2021-08-25 22:57:37 -0400

    save last test

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -82,3 +82,13 @@ test('adds a potion to the inventory', () => {
   
     expect(player.inventory.length).toBeGreaterThan(oldCount);
 });
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
